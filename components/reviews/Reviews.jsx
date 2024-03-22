32c303d6ee520cbd8e6f755d37bd88528d2c687e
David-MacD <david-p-macdonald@outlook.com>
2024-03-22 10:52:07 +0000

    Minor styling

--- components/reviews/Reviews.jsx
+++ components/reviews/Reviews.jsx
@@ -4,13 +4,6 @@ import "./Reviews.css"
 import { useState, useEffect } from "react";
 import useSWR from 'swr';
 
-// async function Review({ country }) {
-//   return (
-//     <p className="review__author">
-//       {text}
-//     </p>
-//   )
-// }
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 const useReview = (country) => {
@@ -30,24 +23,7 @@ const useReview = (country) => {
 function content__toggler() {
 
   const [currentCountry, setCurrentCountry] = useState(null);
-  // const [review, setReview] = useState({});
-  // const { review, error, isLoading } = useSWR(buildURL(currentCountry), fetcher)
   const { review, error, isLoading } = useReview(currentCountry);
-  console.log("review is", review)
-  // if (error) return <div>failed to load</div>
-  // if (isLoading) return <div>loading...</div>
-
-  // // render data
-  // return <div>hello {data.name}!</div>
-
-
-  // useEffect(() => {
-  //   if (currentCountry) {
-  //     fetch(`https://seal-app-336e8.ondigitalocean.app/reviews?country=${currentCountry}`)
-  //       .then(response => response.json())
-  //       .then(setReview);
-  //   }
-  // }, [currentCountry]);
 
   const handleClick = (ev) => {
     setCurrentCountry(ev.target.textContent.toLowerCase())
@@ -56,13 +32,12 @@ function content__toggler() {
   return (
     <div className="content__toggler-wrapper">
       <h1 className="content__toggler-title">Trusted.</h1>
-      <p className="content__toggler-desc">We've got thousands of happy customers all over the UK. Choose your country to see the latest review:</p>
+      <p className="content__toggler-desc">We've got thousands of happy customers all over the UK</p>
       <div className="content__toggler-buttons">
         <button onClick={handleClick} className={currentCountry === "england" ? "content__toggler-button button-england active" : "content__toggler-button button-england"}>England</button>
         <button onClick={handleClick} className={currentCountry === "wales" ? "content__toggler-button button-wales active" : "content__toggler-button button-wales"}>Wales</button>
         <button onClick={handleClick} className={currentCountry === "scotland" ? "content__toggler-button button-scotland active" : "content__toggler-button button-scotland"}>Scotland</button>
       </div>
-      {/* <div className={!currentCountry ? "hide__review" : "review__wrapper"}> */}
       <div className="review__wrapper">
         <p className="review__text">
           {
