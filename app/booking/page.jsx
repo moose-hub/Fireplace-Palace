f12a7a9f9613cb29ae33a4fe5c3d9727bc6d3e3c
David-MacD <david-p-macdonald@outlook.com>
2024-03-26 14:09:43 +0000

    Add the booking banner

--- app/booking/page.jsx
+++ app/booking/page.jsx
@@ -34,85 +34,88 @@ function Booking() {
     }
   }
   return (
-    <form
-      className="booking__form"
-      action="api/booking"
-      method="post"
-      onSubmit={handleSubmit}
-    >
-      <h2 className="booking__form-heading">Personal Information:</h2>
-      <ul className="booking__form-section">
-        <label htmlFor="name" className="booking__form-label">
-          Full Name
-        </label>
-        <input
-          id="name"
-          type="text"
-          className="booking__form-input"
-          onBlur={handleChange}
-          required
-          pattern="^[A-Z][a-z]+(?: [A-Z][a-z]+)*$"
-        />
-        <label htmlFor="postcode" className="booking__form-label">
-          Postcode
-        </label>
-        <input
-          id="postcode"
-          type="text"
-          className="booking__form-input"
-          onBlur={handleChange}
-          required
-          pattern="^([A-Z][A-HJ-Y]?\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})$"
-        />
-        <label htmlFor="address" className="booking__form-label">
-          House/Flat number and Street Name
-        </label>
-        <input
-          id="address"
-          type="text"
-          className="booking__form-input"
-          onBlur={handleChange}
-          required
-          pattern="(.|\s)*\S(.|\s)*"
-        />
-        <label htmlFor="city" className="booking__form-label">
-          City
-        </label>
-        <input
-          id="city"
-          type="text"
-          className="booking__form-input"
-          onBlur={handleChange}
-        />
-      </ul>
-      <h2 className="booking__form-heading">Contact Information:</h2>
-      <ul className="booking__form-section">
-        <label htmlFor="phone" className="booking__form-label">
-          Phone Number
-        </label>
-        <input
-          id="phone"
-          type="text"
-          className="booking__form-input"
-          onBlur={handleChange}
-          required
-        />
-        <label htmlFor="email" className="booking__form-label">
-          Email
-        </label>
-        <input
-          id="email"
-          type="email"
-          required
-          className="booking__form-input"
-          onBlur={handleChange}
-        />
-      </ul>
-      <div className="booking__form-validation">{validationMessage}</div>
-      <button type="submit" className="booking__form-submit">
-        Request Design Consultation
-      </button>
-    </form>
+    <>
+      <section className="booking__banner">Design Booking</section>
+      <form
+        className="booking__form"
+        action="api/booking"
+        method="post"
+        onSubmit={handleSubmit}
+      >
+        <h2 className="booking__form-heading">Personal Information:</h2>
+        <ul className="booking__form-section">
+          <label htmlFor="name" className="booking__form-label">
+            Full Name
+          </label>
+          <input
+            id="name"
+            type="text"
+            className="booking__form-input"
+            onBlur={handleChange}
+            required
+            pattern="^[A-Z][a-z]+(?: [A-Z][a-z]+)*$"
+          />
+          <label htmlFor="postcode" className="booking__form-label">
+            Postcode
+          </label>
+          <input
+            id="postcode"
+            type="text"
+            className="booking__form-input"
+            onBlur={handleChange}
+            required
+            pattern="^([A-Z][A-HJ-Y]?\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})$"
+          />
+          <label htmlFor="address" className="booking__form-label">
+            House/Flat number and Street Name
+          </label>
+          <input
+            id="address"
+            type="text"
+            className="booking__form-input"
+            onBlur={handleChange}
+            required
+            pattern="(.|\s)*\S(.|\s)*"
+          />
+          <label htmlFor="city" className="booking__form-label">
+            City
+          </label>
+          <input
+            id="city"
+            type="text"
+            className="booking__form-input"
+            onBlur={handleChange}
+          />
+        </ul>
+        <h2 className="booking__form-heading">Contact Information:</h2>
+        <ul className="booking__form-section">
+          <label htmlFor="phone" className="booking__form-label">
+            Phone Number
+          </label>
+          <input
+            id="phone"
+            type="text"
+            className="booking__form-input"
+            onBlur={handleChange}
+            required
+          />
+          <label htmlFor="email" className="booking__form-label">
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            required
+            className="booking__form-input"
+            onBlur={handleChange}
+          />
+        </ul>
+        <div className="booking__form-validation">{validationMessage}</div>
+        <button type="submit" className="booking__form-submit">
+          Request Design Consultation
+        </button>
+      </form>
+    </>
   );
 }
 
