2e5b57bfc8a7fea58056d8a8a3c2f7c059601336
David-MacD <david-p-macdonald@outlook.com>
2024-03-26 14:02:23 +0000

    Add a POST handler for bookings

---
+++ app/api/booking/route.js
@@ -0,0 +1,4 @@
+export async function POST(req) {
+  // Handle booking post
+  return new Response("Your design consultation has been booked!");
+}
--- app/booking/page.jsx
+++ app/booking/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./page.css";
 
 function Booking() {
@@ -9,15 +9,15 @@ function Booking() {
     address: "",
     city: "",
     phone: "",
-    email: ""
+    email: "",
   });
 
   function handleChange(e) {
     const { id, value } = e.target;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [id]: value
-    }))
+      [id]: value,
+    }));
   }
 
   const [validationMessage, setValidationMessage] = useState("");
@@ -30,40 +30,83 @@ function Booking() {
     } else {
       setValidationMessage("");
 
-      console.log(`Form data: ${formData}`);
+      console.log(`Form data: ${JSON.stringify(formData)}`);
     }
   }
   return (
-    <form className="booking__form" action="/booking" method="post" onSubmit={handleSubmit}>
+    <form
+      className="booking__form"
+      action="api/booking"
+      method="post"
+      onSubmit={handleSubmit}
+    >
       <h2 className="booking__form-heading">Personal Information:</h2>
       <ul className="booking__form-section">
         <label htmlFor="name" className="booking__form-label">
           Full Name
         </label>
-        <input id="name" type="text" className="booking__form-input" onChange={handleChange} required pattern="^[A-Z][a-z]+(?: [A-Z][a-z]+)*$"/>
+        <input
+          id="name"
+          type="text"
+          className="booking__form-input"
+          onBlur={handleChange}
+          required
+          pattern="^[A-Z][a-z]+(?: [A-Z][a-z]+)*$"
+        />
         <label htmlFor="postcode" className="booking__form-label">
           Postcode
         </label>
-        <input id="postcode" type="text" className="booking__form-input" onChange={handleChange} required pattern="^([A-Z][A-HJ-Y]?\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})$"/>
+        <input
+          id="postcode"
+          type="text"
+          className="booking__form-input"
+          onBlur={handleChange}
+          required
+          pattern="^([A-Z][A-HJ-Y]?\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})$"
+        />
         <label htmlFor="address" className="booking__form-label">
           House/Flat number and Street Name
         </label>
-        <input id="address" type="text" className="booking__form-input" onChange={handleChange} required pattern="(.|\s)*\S(.|\s)*"/>
+        <input
+          id="address"
+          type="text"
+          className="booking__form-input"
+          onBlur={handleChange}
+          required
+          pattern="(.|\s)*\S(.|\s)*"
+        />
         <label htmlFor="city" className="booking__form-label">
           City
         </label>
-        <input id="city" type="text" className="booking__form-input" onChange={handleChange} />
+        <input
+          id="city"
+          type="text"
+          className="booking__form-input"
+          onBlur={handleChange}
+        />
       </ul>
       <h2 className="booking__form-heading">Contact Information:</h2>
       <ul className="booking__form-section">
         <label htmlFor="phone" className="booking__form-label">
           Phone Number
         </label>
-        <input id="phone" type="text" className="booking__form-input" onChange={handleChange} required/>
+        <input
+          id="phone"
+          type="text"
+          className="booking__form-input"
+          onBlur={handleChange}
+          required
+        />
         <label htmlFor="email" className="booking__form-label">
           Email
         </label>
-        <input id="email" type="email" required className="booking__form-input" onChange={handleChange} />
+        <input
+          id="email"
+          type="email"
+          required
+          className="booking__form-input"
+          onBlur={handleChange}
+        />
       </ul>
       <div className="booking__form-validation">{validationMessage}</div>
       <button type="submit" className="booking__form-submit">
