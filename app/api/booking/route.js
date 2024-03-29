5e625d84557b677d9858db2a89a33295c3d797fb
moose-hub <infamollagames@gmail.com>
2024-03-29 03:17:35 +0000

    finished booking form

--- app/api/booking/route.js
+++ app/api/booking/route.js
@@ -1,4 +1,33 @@
-export async function POST(req) {
-  // Handle booking post
-  return new Response("Your design consultation has been booked!");
+import { readFile, writeFile } from "node:fs/promises";
+import path from "path";
+
+// Define the file relatively for accuracy
+const filepath = path.join(process.cwd(), "app/api/booking/bookingData.json");
+
+// extracted function to handle post requests
+export async function POST (req) {
+  try {
+    const body = await req.json();
+    const file = await readFile(filepath, "utf8");
+    const data = JSON.parse(file);
+    
+    data.push(body);
+    await writeFile(filepath, JSON.stringify(data, null, 2), "utf8");
+    
+    return Response.json({ success: true, payload: { msg: "Consultation Booked!" }});
+  } catch (error) {
+    console.error(error);
+    return Response.json({ success: false, error: "Internal Server Error" });
+  }
 }
+
+// import read/write file from fs    ***
+// assign filepath to variable      ***
+// define function (req, res) to handle our post request    ***
+// try catch for graceful error handling    ***
+// get data from req.json and assign to variable    ***
+// parse current data from json file    ***
+// add our data to the existing data with push method    ***
+// write new data to json docustore    ***
+// success response status and message to be handled on frontend    ***
+// fail response status and message to be handled on frontend    ***
